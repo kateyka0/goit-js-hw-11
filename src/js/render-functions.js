74e60d5c10254e21cc5bc5a461df226b34f3c37a
Katrin <kateyka1410@gmail.com>
2024-06-17 18:55:50 +0300

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,91 +1,36 @@
-// import iziToast from 'izitoast';
-// // Додатковий імпорт стилів
-// import 'izitoast/dist/css/iziToast.min.css';
 
-// // Описаний у документації
-// import SimpleLightbox from 'simplelightbox';
-// // Додатковий імпорт стилів
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-
-// let gallery = new SimpleLightbox('.gallery li a');
-// gallery.on('show.simplelightbox', function () {
-//   // do something…
-// });
-
-// gallery.on('error.simplelightbox', function (e) {
-//   console.log(e); // some usefull information
-// });
-// export function showQueryError(error) {
-//   console.log(error);
-//   iziToast.show({
-//     title: '',
-//     icon: 'icon-person',
-//     color: 'red', // blue, red, green, yellow
-//     position: 'topRight', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
-//     message:
-//       'Sorry, there are no images matching your search query. Please try again!',
-//   });
-// }
-// export function renderPhotoList(photos, container) {
-//   console.log(photos);
-//   const markup = photos.hits
-//     .map(photo => {
-//       return `<li>
-//                 <a href="${photo.largeImageURL}"><img src='${photo.webformatURL}' alt='${photo.tags}'></a>
-//                 <div class="content">
-//                     <div class="item"><h3>Likes</h3><p>${photo.likes}</p></div>
-//                     <div class="item"><h3>Views</h3><p>${photo.views}</p></div>
-//                     <div class="item"><h3>Comments</h3><p>${photo.comments}</p></div>
-//                     <div class="item"><h3>Downloads</h3><p>${photo.downloads}</p></div>
-//                 </div>
-//             </li>`;
-//     })
-//     .join('');
-//   container.textContent = '';
-//   container.insertAdjacentHTML('beforeend', markup);
-//   gallery.refresh();
-// }
-
-export function clearGallery() {
-    const gallery = document.querySelector('.gallery');
-    gallery.innerHTML = '';
+const refs = {
+imageForm: document.querySelector('.form'),
+imageInput: document.querySelector('.input'),
+submitButton: document.querySelector('.btn'),
+imageList: document.querySelector('.images-list'),
 }
 
-export function displayImages(images) {
-    const gallery = document.querySelector('.gallery');
-    images.forEach(image => {
-        const card = createImageCard(image);
-        gallery.appendChild(card);
-    });
-}
+export function renderImages(images) {
 
-function createImageCard(image) {
-    const card = document.createElement('div');
-    card.classList.add('card');
+    const markup = images.hits.map((image) => {
+       return `<li class="images-list-item">
+  <a class="img-link" href=${image.largeImageURL} onclick="event.preventDefault()"><img class="img" src=${image.webformatURL} alt=${image.tags}></img></a>
+   <ul class="img-dscr">
+      <li class="img-data">
+        <p class="img-data-title">Likes</p>
+        <p class="img-data-numbers">${image.likes}</p>
+      </li>
+      <li class="img-data">
+        <p class="img-data-title">Views</p>
+        <p class="img-data-numbers">${image.views}</p>
+      </li>
+      <li class="img-data">
+        <p class="img-data-title">Comments</p>
+        <p class="img-data-numbers">${image.comments}</p>
+      </li>
+      <li class="img-data">
+        <p class="img-data-title">Downloads</p>
+        <p class="img-data-numbers">${image.downloads}</p>
+      </li>
+    </ul>
+</li>`;
+    }).join('\n');
 
-    const imageElement = document.createElement('img');
-    imageElement.src = image.webformatURL;
-    imageElement.alt = image.tags;
-
-    const cardInfo = document.createElement('div');
-    cardInfo.classList.add('card-info');
-    cardInfo.innerHTML = `
-        <p>Likes: ${image.likes}</p>
-        <p>Views: ${image.views}</p>
-        <p>Comments: ${image.comments}</p>
-        <p>Downloads: ${image.downloads}</p>
-    `;
-
-    card.appendChild(imageElement);
-    card.appendChild(cardInfo);
-
-    return card;
-}
-
-export function showMessage(message) {
-    iziToast.error({
-        title: 'Error',
-        message: message,
-        position: 'topRight'
-    });
+    refs.imageList.innerHTML = markup;
 }
